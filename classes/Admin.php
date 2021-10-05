7b67b6d49e8dd9f1b7d81469ec7244abbf23a715
utso <cccygodev@gmail.com>
2021-10-05 18:58:55 +0200

    Begin outline of CDB upload form

--- classes/Admin.php
+++ classes/Admin.php
@@ -2,6 +2,8 @@
 
 namespace bye_plugin;
 
+use SQLite3;
+
 class Admin
 {
     private Database $database;
@@ -17,7 +19,32 @@ class Admin
 
     function admin_page() {
         if (current_user_can('manage_options')) {
-            if (isset($_POST['code'])) {
+            if (isset($_FILES['cdb'])) {
+                try {
+                    $cdb = new SQLite3($_FILES['cdb']['tmp_name']);
+                    $cards = $cdb->query('SELECT id, name FROM texts;');
+                    ?>
+
+                    <form method="POST">
+
+                    <?php
+
+                    foreach ($cards->fetchArray(SQLITE3_ASSOC) as $card) {
+                        ?>
+                        <div><input name="<?= $card['id'] ?>" type="checkbox"/><span><?= $card['name'] ?></span></div>
+                        <?php
+                    }
+
+                    ?>
+                    </form>
+                    <?php
+                }
+                catch (Exception $e) {
+                    //TODO: handle
+                    echo($e->getMessage());
+                }
+            }
+            elseif (isset($_POST['code'])) {
                 $expansion_id = $this->database->find_expansion($_POST['expansion'])->id;
                 if (is_null($expansion_id)) {
                    $expansion_id = $this->database->create_expansion($_POST['expansion'], $_POST['expansion']);
@@ -34,47 +61,25 @@ class Admin
 
                 echo "<p> Card {$card_insert_res} inserted </p>";
             }
+            else { ?>
+                <form enctype="multipart/form-data" method="POST">
+                    <div>
+                        <label for="t_code" style="display:inline-block;width:16ch">CardID</label>
+                        <input id="t_code" name="code" type="text" required/>
+                        <label for="t_version" style="display:inline-block;width:16ch">Version</label>
+                        <input id="t_version" name="version" type="text" required/>
+                        <label for="t_expansion" style="display:inline-block;width:16ch">Expansion Code</label>
+                        <input id="t_expansion" name="expansion" type="text" required/>
+                    </div>
+                    <div>
+                        <input type="hidden" name="MAX_FILE_SIZE" value="10000000"/>
+                        <label for="u_cdb" style="display:inline-block;width:16ch">CDB File</label>
+                        <input id="u_cdb" name="cdb" type="file" required>
+                    </div>
+                    <?php submit_button() ?>
+                </form>
+                <?php
+            }
         }
-        ?>
-
-
-        <form method="POST">
-            <div>
-                <label for="code" style="display:inline-block;width:16ch">CardID</label>
-                <input name="code" type="text" required/>
-                <label for="version" style="display:inline-block;width:16ch">Version</label>
-                <input name="version" type="text" required/>
-                <label for="expansion" style="display:inline-block;width:16ch">Expansion Code</label>
-                <input name="expansion" type="text" required/>
-            </div>
-            <div>
-                <label for="name" style="display:inline-block;width:16ch">Name</label>
-                <input name="name" type="text" required/>
-                <label for="type"style="display:inline-block;width:16ch">Card Type</label>
-                <input name="type" type="text" required/>
-                <label for="image" style="display:inline-block;width:16ch">Image</label>
-                <input name="image" type="file"/>
-            </div>
-            <div>
-                <label for="race" style="display:inline-block;width:16ch">Type</label>
-                <input name="race" type="text"/>
-                <label for="attribute" style="display:inline-block;width:16ch">Attribute</label>
-                <input name="attribute" type="text"/>
-                <label for="level" style="display:inline-block;width:16ch">Level</label>
-                <input name="level" type="text"/>
-            </div>
-            <div>
-                <label for="atk" style="display:inline-block;width:16ch">ATK</label>
-                <input name="atk" type="text"/>
-                <label for="def" style="display:inline-block;width:16ch">DEF</label>
-                <input name="def" type="text"/>
-            </div>
-            <div>
-                <label for="description" style="display:inline-block;width:16ch">Card Text</label>
-                <textarea name="description" style="width:100ch"></textarea>
-            </div>
-            <?php submit_button() ?>
-        </form>
-        <?php
     }
 }
