04cb38c7ccbbebff6fd6e9b00ec489388a620930
utso <cccygodev@gmail.com>
2021-10-21 15:43:55 +0200

    Move card selection in block editor to sidebar

--- src/blocks/bye-cardviewer-card/edit.js
+++ src/blocks/bye-cardviewer-card/edit.js
@@ -1,4 +1,5 @@
-import {useBlockProps} from '@wordpress/block-editor';
+import {useBlockProps, InspectorControls} from '@wordpress/block-editor';
+import {PanelBody} from '@wordpress/components';
 import {useState, useEffect} from '@wordpress/element';
 
 const _siteUrl = 'https://bye-project.xyz'; //better replace this with a get_site_url passed in from PHP
@@ -24,41 +25,62 @@ export const edit = function ({attributes, setAttributes}) {
     useEffect(() => {
         wp.apiFetch({path: 'bye/v1/expansions'}).then(data => {
             setExpansions(data);
-        }, error => { console.log(attributes.cardId + " / " + error) });
+        }, error => {
+            console.log(attributes.cardId + " / " + error)
+        });
         if (attributes.expansion) {
             wp.apiFetch({path: 'bye/v1/cards/' + attributes.expansion}).then(data => {
-                setCards(data.sort((a,b) => a.code - b.code));
-            }, error => { console.log(attributes.cardId + " / " + error) })
+                setCards(data.sort((a, b) => a.code - b.code));
+            }, error => {
+                console.log(attributes.cardId + " / " + error)
+            })
         }
     }, []);
 
     return <div {...blockProps}>
-        <select {...{
-            placeholder: 'Expansion', value: attributes.expansion, disabled: (expansions.length === 0), onChange: function (event) {
-                setAttributes({expansion: event.target.value})
-                wp.apiFetch({path: 'bye/v1/cards/' + event.target.value}).then(data => {
-                    setCards(data.sort((a,b) => a.code - b.code));
-                })
-            }
-        }}>
-            {expansions.map((expansion) => {
-                return <option {...{value: expansion.code}}>{expansion.name}</option>
-            })}
-        </select>
-        <select {...{
-            placeholder: 'CardID', value: attributes.cardId, disabled: (cards.length === 0), onChange: function (event) {
-                setAttributes({cardId: event.target.value})
-            }
-        }}>
-            {cards.map((card) => {
-                return <option {...{value: card.code}}>{card.name}</option>
-            })}
-        </select>
-        <input {...{
-            placeholder: 'Max. version', value: attributes.version, onChange: function (event) {
-                setAttributes({version: event.target.value})
-            }
-        }}/>
+        <InspectorControls>
+            <PanelBody title={'Card Selection'} initialOpen={true}>
+                <fieldset>
+                    <legend>Expansion</legend>
+                    <select {...{
+                        value: attributes.expansion,
+                        disabled: (expansions.length === 0),
+                        onChange: function (event) {
+                            setAttributes({expansion: event.target.value})
+                            wp.apiFetch({path: 'bye/v1/cards/' + event.target.value}).then(data => {
+                                setCards(data.sort((a, b) => a.code - b.code));
+                            })
+                        }
+                    }}>
+                        {expansions.map((expansion) => {
+                            return <option {...{value: expansion.code}}>{expansion.name}</option>
+                        })}
+                    </select>
+                </fieldset>
+                <fieldset>
+                    <legend>Card</legend>
+                    <select {...{
+                        value: attributes.cardId,
+                        disabled: (cards.length === 0),
+                        onChange: function (event) {
+                            setAttributes({cardId: event.target.value})
+                        }
+                    }}>
+                        {cards.map((card) => {
+                            return <option {...{value: card.code}}>{card.name}</option>
+                        })}
+                    </select>
+                </fieldset>
+                <fieldset>
+                    <legend>Max. Version</legend>
+                    <input {...{
+                        value: attributes.version, onChange: function (event) {
+                            setAttributes({version: event.target.value})
+                        }
+                    }}/>
+                </fieldset>
+            </PanelBody>
+        </InspectorControls>
         <img {...{src: imgUrl}}
              alt="Preview Image"/>
     </div>
